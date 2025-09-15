df5e9a83366f8c25fa06492608214b52f7351ed5
AdityaK0 <aditya123905@gmail.com>
2025-09-15 12:44:32 +0530

    check 2 in pvt repo

--- apps/products/serializers.py
+++ apps/products/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Product, ProductImage, Category
 
+
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
