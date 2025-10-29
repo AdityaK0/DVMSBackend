cf3cf14bc048a9eee4d8d3f6b1825429938474c9
AdityaK0 <aditya123905@gmail.com>
2025-10-29 16:09:19 +0530

    check

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -29,18 +29,3 @@ urlpatterns = [
     path("vendor/filter/", views.filter_products, name="filter_products"),
 ]
 
-# from django.urls import path
-# from .views import (
-#     ProductListView, ProductDetailView, ProductCreateView,
-#     ProductUpdateView, ProductDeleteView, CategoryListView,
-#     VendorCatalogView
-# )
-
-# urlpatterns = [
-#     path('', ProductListView.as_view(), name='product-list'),
-#     path('create/', ProductCreateView.as_view(), name='product-create'),
-#     path('<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
-#     path('<int:pk>/update/', ProductUpdateView.as_view(), name='product-update'),
-#     path('<int:pk>/delete/', ProductDeleteView.as_view(), name='product-delete'),
-#     path('categories/', CategoryListView.as_view(), name='category-list'),
-# ]
