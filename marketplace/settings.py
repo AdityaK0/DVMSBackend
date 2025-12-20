6a2ee26cfd98f3521397694ea87aa85839bbb006
AdityaK0 <aditya123905@gmail.com>
2025-12-20 16:47:17 +0530

    auth increment count

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -261,7 +261,7 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(hours=3),  # Reduced from 10 hours for security
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=5),  # Reduced from 10 hours for security
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
